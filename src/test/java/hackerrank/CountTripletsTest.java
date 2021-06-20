d806c54bdd9f34b12721a6bd956b1f2c53a3203c
carlos.ueda <carlos.ueda@gmail.com>
2021-06-20 07:01:01 -0400

    Test using AssertJ

--- src/test/java/hackerrank/CountTripletsTest.java
+++ src/test/java/hackerrank/CountTripletsTest.java
@@ -1,11 +1,10 @@
 package hackerrank;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
-
+import static org.assertj.core.api.Assertions.*;
 import hackerrank.CountTriplets;
 
 class CountTripletsTest {
@@ -13,18 +12,18 @@ class CountTripletsTest {
 	@Test
 	void testCountTriplets_shouldReturn6() {
 		long result = CountTriplets.countTriplets(Arrays.asList(new Long[] {1L, 3L, 9L, 9L, 27L, 81L}), 3);
-		assertEquals(6, result);
+		assertThat(result).isEqualTo(6);
 	}
 	
 	@Test
 	void testCountTriplets_shouldReturn4() {
 		long result = CountTriplets.countTriplets(Arrays.asList(new Long[] {1L, 5L, 5L, 25L, 125L}), 5);
-		assertEquals(4, result);
+		assertThat(result).isEqualTo(4);
 	}
 	
 	@Test
 	void testCountTriplets_shouldReturn() {
 		long result = CountTriplets.countTriplets(Arrays.asList(new Long[] {1L, 1L, 1L, 1L}), 1);
-		assertEquals(4, result);
+		assertThat(result).isEqualTo(4);
 	}
 }
