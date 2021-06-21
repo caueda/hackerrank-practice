f369e51cf7a5b0db1271a68b6987fadd6d800304
carlos.ueda <carlos.ueda@gmail.com>
2021-06-21 07:52:12 -0400

    Refatoctoring BalancedBracketsTest

--- src/test/java/hackerrank/BalancedBracketsTest.java
+++ src/test/java/hackerrank/BalancedBracketsTest.java
@@ -1,21 +1,24 @@
 package hackerrank;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 import hackerrank.BalancedBrackets;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BalancedBracketsTest {
-	
-	@Test
-	void testNoBalancedExpression() {
-		String result = BalancedBrackets.isBalanced("}][}}(}][))]");
-		assertEquals("NO", result);
+
+	@ParameterizedTest
+	@ValueSource(strings = {"[()[]{}]", "{([])}"})
+	void parameterizedBalanceExpressionAssertTrue(String input) {
+		assertThat(BalancedBrackets.isBalanced(input)).isEqualTo("YES");
 	}
-	
-	@Test
-	void testBalancedExpression() {
-		String result = BalancedBrackets.isBalanced("[{(())}]");
-		assertEquals("YES", result);
+
+	@ParameterizedTest
+	@ValueSource(strings = {"}][}}(}][))]", "{([])}("})
+	void parameterizedBalanceExpressionAssertFalse(String input) {
+		assertThat(BalancedBrackets.isBalanced(input)).isEqualTo("NO");
 	}
 }
