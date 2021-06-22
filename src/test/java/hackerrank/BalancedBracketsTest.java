08511cb9f62f91a0fa8755118053f8d9a835824d
carlos.ueda <carlos.ueda@gmail.com>
2021-06-22 06:57:42 -0400

    BalancedBracketsTest with CSV parameter

--- src/test/java/hackerrank/BalancedBracketsTest.java
+++ src/test/java/hackerrank/BalancedBracketsTest.java
@@ -2,10 +2,12 @@ package hackerrank;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import hackerrank.BalancedBrackets;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class BalancedBracketsTest {
@@ -20,5 +22,12 @@ class BalancedBracketsTest {
 	@ValueSource(strings = {"}][}}(}][))]", "{([])}("})
 	void parameterizedBalanceExpressionAssertFalse(String input) {
 		assertThat(BalancedBrackets.isBalanced(input)).isEqualTo("NO");
+	}
+
+	@DisplayName("Balanced Brackets (CSV input)")
+	@ParameterizedTest(name=" {displayName} - [{index}] {arguments}")
+	@CsvFileSource(resources = "/inputBalancedBrackets.csv", numLinesToSkip = 1)
+	void balancedBracketsFromCSVTest(String expression, String result) {
+		assertThat(BalancedBrackets.isBalanced(expression)).isEqualTo(result);
 	}
 }
